2422c8d24655a3e701f9c72d34b58646bfdfba27
SOHAM2099 <iamsohamsamanta2003.2@gmail.com>
2025-07-02 02:45:33 +0530

    Time: 2 ms (71.32%), Space: 45.6 MB (32.72%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.java
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -1,7 +1,7 @@
 class Solution {
     //SOHAM2099
     public boolean validMountainArray(int[] arr) {
-        int slope=0;
+       /* int slope=0;
         if(arr.length<3)
             return false;
         if(arr[1]<=arr[0])              //no up-slope
@@ -17,6 +17,25 @@ class Solution {
         }
         if(slope==0)                    //no down-slope
             return false;
-        return true;
+        return true;*/
+
+        if (arr.length < 3)
+            return false;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        // Move left pointer up while strictly increasing
+        while (left + 1 < arr.length && arr[left] < arr[left + 1])
+            left++;
+
+        
+
+        // Move right pointer down while strictly decreasing
+        while (right - 1 >= 0 && arr[right - 1] > arr[right])
+            right--;
+        
+        // Both pointers must meet at the peak and not be at the ends
+        return left > 0 && right < arr.length - 1 && left == right;
     }
 }

2ad10bbbe358b10f487f3a195a7cbf717d8a03b0
SOHAM2099 <iamsohamsamanta2003.2@gmail.com>
2025-07-03 02:31:08 +0530

    Time: 2 ms (80.5%), Space: 46.4 MB (20.81%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    //SOHAM2099
+    public void moveZeroes(int[] nums) {
+        
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[k]=nums[i];
+                if(k!=i)    
+                    nums[i]=0;
+                k++;
+            }
+        }
+    }
+}
